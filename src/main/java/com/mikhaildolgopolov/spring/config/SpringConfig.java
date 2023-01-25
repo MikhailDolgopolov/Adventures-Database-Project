18544dbbb52e782b6744f53588b44a1546e1df5d
Михаил Долгополов <mishad2304@gmail.com>
2023-01-25 22:23:26 +0300

    Loading fragments

--- src/main/java/com/mikhaildolgopolov/spring/config/SpringConfig.java
+++ src/main/java/com/mikhaildolgopolov/spring/config/SpringConfig.java
@@ -25,6 +25,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.spring6.view.ThymeleafViewResolver;
 import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 import org.thymeleaf.templateresolver.ITemplateResolver;
 
 import javax.sql.DataSource;
@@ -39,14 +40,14 @@ public class SpringConfig implements WebMvcConfigurer {
             "/js/**",
             "/webjars/**",
             "/static/**",
-            "/resources/",
+            "/resources/**",
             "/**", "/trips/**","/trips/add/",
-            "/trip/**", "/fragments/"
+            "/trip/**", "/templates/fragments/"
     };
     private static final String[] RESOURCE_LOCATIONS = {
             "classpath:/META-INF/resources/", "classpath:/resources/",
             "classpath:/static/", "classpath:/resources/static/", "classpath:/templates/",
-            "classpath:/fragments"};
+            "classpath:/resources/fragments/"};
     private final ApplicationContext applicationContext;
 
     @Autowired
@@ -70,15 +71,31 @@ public class SpringConfig implements WebMvcConfigurer {
         resolver.setViewNames(new String[]{"*.html"});
         return resolver;
     }
-
-    private ITemplateResolver htmlTemplateResolver() {
+    @Bean
+    public ITemplateResolver htmlTemplateResolver() {
         SpringResourceTemplateResolver resolver
                 = new SpringResourceTemplateResolver();
         resolver.setApplicationContext(applicationContext);
-        resolver.setPrefix("/WEB-INF/views/");
+        resolver.setPrefix("classpath:/templates/fragments/");
+        resolver.setSuffix(".html");
         resolver.setCacheable(false);
+        resolver.setCharacterEncoding("UTF-8");
+        resolver.setOrder(1);
         resolver.setTemplateMode(TemplateMode.HTML);
+        resolver.setCheckExistence(true);
         return resolver;
+    }
+    @Bean
+    public ITemplateResolver secondaryTemplateResolver() {
+        ClassLoaderTemplateResolver secondaryTemplateResolver = new ClassLoaderTemplateResolver();
+        secondaryTemplateResolver.setPrefix("classpath:/templates/");
+        secondaryTemplateResolver.setSuffix(".html");
+        secondaryTemplateResolver.setTemplateMode(TemplateMode.HTML);
+        secondaryTemplateResolver.setCharacterEncoding("UTF-8");
+        secondaryTemplateResolver.setOrder(2);
+        secondaryTemplateResolver.setCheckExistence(true);
+
+        return secondaryTemplateResolver;
     }
     @Bean
     public DataSource dataSource(){
