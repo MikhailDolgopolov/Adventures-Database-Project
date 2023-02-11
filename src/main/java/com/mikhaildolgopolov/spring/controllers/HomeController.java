fe26aa2673b956127d3785a287fa10fa5c7fb95a
Михаил Долгополов <mishad2304@gmail.com>
2023-02-11 22:27:18 +0300

    React

--- src/main/java/com/mikhaildolgopolov/spring/controllers/HomeController.java
+++ src/main/java/com/mikhaildolgopolov/spring/controllers/HomeController.java
@@ -1,9 +1,11 @@
 package com.mikhaildolgopolov.spring.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@CrossOrigin
 @Controller
 @RequestMapping("/home/")
 public class HomeController {
